f6df01e494e7859198b72c78b12a612bb7b674be
Julia Solias <juliasohu@gmail.com>
2025-02-04 16:14:28 +0100

    create Navbar + UI update

---
+++ react-rental/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import logo from "../assets/JU-logo.png"
+
+function Navbar(){
+    return <div className="navbar">
+        <img src={logo} alt="Logo" />
+        <h3>APPARTMENTS FOR RENTAL</h3>
+    </div>
+}
+
+export default Navbar
