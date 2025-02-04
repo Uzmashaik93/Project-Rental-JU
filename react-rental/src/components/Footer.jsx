ce1ceb200d65cddfbeb70f813945d2c2b4cf1771
Julia Solias <juliasohu@gmail.com>
2025-02-04 16:30:13 +0100

    style footer

---
+++ react-rental/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div className="footer">
+      <a href="https://github.com/Uzmashaik93/Project-Rental-JU">
+        Link to repository
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
