6206a03a1e2e571c33754c84d6ed2d511eec85ee
uzma shaik <uzma97910@gmail.com>
2025-02-12 14:39:06 +0100

    style: about page css and layout

--- react-rental/src/pages/About.jsx
+++ react-rental/src/pages/About.jsx
@@ -1,22 +1,47 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
-function About() {
+const teamMembers = [
+  {
+    name: "Shaik Uzma",
+    github: "https://github.com/alicejohnson",
+    linkedin: "https://www.linkedin.com/in/alicejohnson/",
+  },
+  {
+    name: "Julia solias",
+    github: "https://github.com/juliasohu",
+    linkedin: "https://www.linkedin.com/in/julia-solias-huelamo/",
+  },
+];
+function AboutPage() {
   return (
-    <div className="about">
-      <h2>About Page</h2>
-      <p>
-        JU Appartments is a page where you can find the best bookings available.
-      </p>
-      <p>
-        This page was created by Uzma Shaik and Julia Solias. You'll find our
-        contact details below.
-      </p>
-      <h3>Github</h3>
-      <a href="https://github.com/Uzmashaik93">Uzma</a>
-      <a href="https://github.com/juliasohu">Julia</a>
-      <h3>Linkedin</h3>
-      <a href=" https://www.linkedin.com/in/shaik-uzma-1b6b86349/">Uzma</a>
-      <a href="https://www.linkedin.com/in/julia-solias-huelamo/">Julia</a>
+    <div className="about-container">
+      <div>
+        <h2>About Page</h2>
+        <p>
+          JU Appartments is a page where you can find the best bookings
+          available.
+        </p>
+        <p>
+          This page was created by Uzma Shaik and Julia Solias. You'll find our
+          contact details below.
+        </p>
+      </div>
+      {teamMembers.map((member, index) => (
+        <div className="member-card" key={index}>
+          <h3>{member.name}</h3>
+          <div className="social-links">
+            <a href={member.github} target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faGithub} size="2x" />
+            </a>
+            <a href={member.linkedin} target="_blank" rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faLinkedin} size="2x" />
+            </a>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
-export default About;
+
+export default AboutPage;
